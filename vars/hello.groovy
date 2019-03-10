81a114d89d22eda61124ae89d69714f5484f8f7a
Sanjay Tripathi <skt_rt@yahoo.co.uk>
2019-03-10 21:25:00 +0000

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,3 @@
 def call(String name) {
-  echo "hello & hi ho he updated this comes from within shared lib function ${name}. Updated to check push behaviour"
+  echo "hello & hi ho he hu updated this comes from within shared lib function ${name}. Updated to check push behaviour"
  }
