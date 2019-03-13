7e4175aac6622e173f84608217b7ad7a8f5ca578
Sanjay Tripathi <skt_rt@yahoo.co.uk>
2019-03-13 21:39:43 +0000

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,8 @@
 def call(String name) {
   echo "hello & hi ho he hu updated this comes from within shared lib function ${name}. Updated to check push behaviour"
  }
-def checkout_code(String branch_name) {
-  checkout([$class: 'GitSCM', branches: [[name: '*/${branch_name}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f0682df-26dd-48f2-86d4-40e2b48eabc7', url: 'git@github.com:SKT-Jenkins/mobile-rep.git']]])
+def checkout_code(String branch_name1) {
+  echo "SANJAY: Incoming value of branch is ${branch_name1}"
+  checkout([$class: 'GitSCM', branches: [[name: '"*/${branch_name1}"']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f0682df-26dd-48f2-86d4-40e2b48eabc7', url: 'git@github.com:SKT-Jenkins/mobile-rep.git']]])
+  
 } 
