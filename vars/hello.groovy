df225030c1e39e24fa04ebea0e282a6f93ffd84c
Sanjay Tripathi <skt_rt@yahoo.co.uk>
2019-03-13 21:04:25 +0000

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,6 @@
 def call(String name) {
   echo "hello & hi ho he hu updated this comes from within shared lib function ${name}. Updated to check push behaviour"
  }
+def checkout_code(String branch_name) {
+  checkout([$class: 'GitSCM', branches: [[name: '*/${branch_name}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f0682df-26dd-48f2-86d4-40e2b48eabc7', url: 'git@github.com:SKT-Jenkins/mobile-rep.git']]])
+}
