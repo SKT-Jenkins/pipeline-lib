661e01d2c1ae39b997c39331797d584f025349b6
Sanjay Tripathi <skt_rt@yahoo.co.uk>
2019-03-14 12:03:49 +0000

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -6,3 +6,20 @@ def checkout_code(String branch_name1) {
   checkout([$class: 'GitSCM', branches: [[name: '"*/${branch_name1}"']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '0f0682df-26dd-48f2-86d4-40e2b48eabc7', url: 'git@github.com:SKT-Jenkins/mobile-rep.git']]])
   
 } 
+def createGitBranch(String projectName, String targetBranch, String sourceBranch) {
+withCredentials([usernamePassword(
+  credentialsId: 'gitlanapitoken',
+  passwordVariable: 'GIT_API_TOKEN',
+  usernameVriable: 'UNUSED')])
+  {
+  def createBranchRequest = "${env.GIT_HTTPS}/api/v4/projects/${projectName}/repository/branches " +
+    "--data-urlencode \"branch=${targetBranch}\" --data-urlencode \"ref=${sourceBranch}\""
+  
+  response = sh (
+    script: 'curl --noproxy "*" -X POST --header "PRIVATE-TOKEN: ${GIT_API_TOKEN}" ' + createBranchRequest,
+    returnStdout: true
+    )
+  return response
+  }
+}
+  
